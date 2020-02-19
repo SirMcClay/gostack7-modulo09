f0d52b52db8e8c962f4e2eeac95db902ec8a6b8d
sirmcclay <mcclay.design@gmail.com>
2020-02-19 12:24:03 -0300

    Auth layout style

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+export const Content = styled.div``;
