da3007fd48fe2bfa2d708b43d7e67d98d635c566
sirmcclay <mcclay.design@gmail.com>
2020-03-03 12:24:16 -0300

    Auth: begin

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,7 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
