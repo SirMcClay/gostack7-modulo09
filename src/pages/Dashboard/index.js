87cfc69e817250c3199cf6f5f75c667ef63a8778
sirmcclay <mcclay.design@gmail.com>
2020-04-29 12:57:51 -0300

    Listing appointments

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,12 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { format, subDays, addDays } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function Dashboard() {
   const [date, setDate] = useState(new Date());
@@ -13,6 +15,14 @@ export default function Dashboard() {
     () => format(date, "d 'de' MMMM", { locale: pt }),
     [date]
   );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+    }
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
